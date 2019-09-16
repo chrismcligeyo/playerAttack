8d8d653cd73364fa1a0a0002071ade653f2967cf
= <=>
2019-09-16 13:39:26 +0300

    working

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false // track if game is running or not. Initially we dont have a running game, see if we should start a new game button or display attack buttons
+        gameIsRunning: false, // track if game is running or not. Initially we dont have a running game, see if we should start a new game button or display attack buttons
+        turns: []
     },
 
     methods: {
@@ -12,20 +13,26 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = []; // clears li items
 
         },
 
         attack: function () {
             /* //code was like this previously, refactored it using calculateDamage function
-            let max = 10;
+            let max = 10; //max damage
             let min =3;
             let damage = Math.max(Math.floor(Math.random() * max) + 1, min);//means get the highest value(max) of random numbers between 10 and 3
             this.monsterHealth -= damage; */
-
+            let damage = this.calculateDamage(3,10);
             //refactored
-            this.monsterHealth -= this.calculateDamage(3,10);
+            this.monsterHealth -= damage;
             // let damage = this.calculateDamage(3, 10);
             // this.monsterHealth -= damage;
+
+             this.turns.unshift({
+               isPlayer: true,
+               text: "Player hit Monster for " + damage
+            });
 
 
             //after dealing damage to the monster check if we won
@@ -33,6 +40,8 @@ new Vue({
                 return; //so as not to execute code anymore. code in
 
             }
+
+            this.monsterAttacks();
 
             /*  //after dealing damage to the monster check if we won //has been refactored above with chckIfWon function
             if(this.monsterHealth <= 0) { // means if you beat the monster
@@ -48,9 +57,9 @@ new Vue({
             this.playerHealth -= damage;
             */
 
-           this.playerHealth -= this.calculateDamage(5,12);
-
-           this.checkIfWon();
+           // this.playerHealth -= this.calculateDamage(5,12);
+           //
+           // this.checkIfWon();
 
            /* //has been refactored above
             if(this.playerHealth <= 0) { // means if you beat the monster
@@ -60,20 +69,27 @@ new Vue({
             } */
         },
         specialAttack: function () {
+            let damage = this.calculateDamage(10,20);
 
-            this.monsterHealth -= this.calculateDamage(10,20);
+            this.monsterHealth -= damage;
             // let damage = this.calculateDamage(3, 10);
             // this.monsterHealth -= damage;
-
+            this.turns.unshift({
+                isPlayer: true,
+                text: "Player hit Monster for " + damage
+            });
 
             //after dealing damage to the monster check if we won
             if (this.checkIfWon()){
                 return; //so as not to execute code anymore. code in
 
             }
-            this.playerHealth -= this.calculateDamage(5,12);
-
-            this.checkIfWon();
+            this.monsterAttacks();
+            // this.playerHealth -= this.calculateDamage(5,12);
+            //
+            // this.checkIfWon(); //the two :: this.playerHealth -= this.calculateDamage(5,12);
+            //             //
+            //             // this.checkIfWon refACTORED TO THIS.MONSTER ATTACKS
 
 
 
@@ -88,12 +104,28 @@ new Vue({
             } else {
                 this.playerHealth =100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: "Player heals for 10"
+            });
+
+            this.monsterAttacks();
 
         },
 
         giveUp: function () {
             this.gameIsRunning = false;
 
+        },
+
+        monsterAttacks: function(){
+            let damage = this.calculateDamage(5,12);
+          this.playerHealth -= damage;
+            this.checkIfWon();
+            this.turns.unshift({
+                isPlayer: false,
+                text: "Monster hits player for " + damage
+            });
         },
 
         calculateDamage: function(min, max) {
